e69a6459a5de89aab9e573a8f8c52b3690eb976b
Erwin <erwinvides@hotmail.com>
2017-12-26 17:15:12 -0600

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const publicPath = path.join(__dirname, '../public')
+
+const express = require('express');
+const port = process.env.PORT || 3000;
+var app = express();
+app.use(express.static(publicPath));
+
+
+app.get('/', (req, res) => {
+    res.send();
+});
+app.listen(port, () => {
+    console.log(`Server is working in port  ${port}`);
+});
