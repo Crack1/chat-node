e9be914d4fc055e4294de8ed927bcd8d97d00d12
Erwin <erwinvides@hotmail.com>
2017-12-26 21:01:49 -0600

    'chat'

--- server/server.js
+++ server/server.js
@@ -1,15 +1,41 @@
 const path = require('path');
-const publicPath = path.join(__dirname, '../public')
+const http = require('http');
+const publicPath = path.join(__dirname, '../public');
+const socketIO = require('socket.io')
 
 const express = require('express');
 const port = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(publicPath));
 
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log("New connection");
+
+    socket.on('disconnect', () => {
+        console.log('User was Disconnected to server');
+    });
+
+
+
+    socket.on('createMessage', (message) => {
+        console.log('Create Message ->', message);
+
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+    });
+});
+
+
 
 app.get('/', (req, res) => {
     res.send();
 });
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is working in port  ${port}`);
 });
